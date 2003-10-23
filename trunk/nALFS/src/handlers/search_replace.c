67bd287bfa98d504669b1711f030408ee322611f
neven <neven@7d5e3409-5ede-0310-83dd-dbcb3a32f907>
2003-10-23 01:21:57 +0000

    Duplicated code from <search_replace> handler removed.

--- trunk/nALFS/src/handlers/search_replace.c
+++ trunk/nALFS/src/handlers/search_replace.c
@@ -50,14 +50,11 @@
 #define TMP_SEARCH_REPLACE_FILE	"/tmp/nALFS-XXXXXX"
 
 
-/* TODO: Only alloc_base_dir(_new) is different. */
-
-int search_replace_main_ver2(element_s *el)
+int search_replace_main(element_s *el, const char *base_dir)
 {
 	int i, c, fdw, offset, num_found = 0;
 	char *buf = NULL;
 	char *file;
-	char *base;
 	char *find;
 	char *replace = El_search_replace_replace(el);
 	char tmp_file[] = TMP_SEARCH_REPLACE_FILE;
@@ -76,21 +73,16 @@ int search_replace_main_ver2(element_s *el)
 		return -1;
 	}
 
-	base = alloc_base_dir(el);
-
-	if (change_current_dir(base)) {
-		xfree(base);
+	if (change_current_dir(base_dir)) {
 		xfree(find);
 		xfree(file);
 		return -1;
 	}
 
-	Nprint_h("Searching %s (%s)",  file, base);
+	Nprint_h("Searching %s (%s)",  file, base_dir);
 	Nprint_h("    for \"%s\"", find);
 	Nprint_h("    and replacing with \"%s\".",
 		replace ? replace : "");
-
-	xfree(base);
 
 	if ((fp = fopen(file, "r")) == NULL) {
 		Nprint_h_err("Opening %s failed: %s",
@@ -190,143 +182,28 @@ int search_replace_main_ver2(element_s *el)
 	return 0;
 }
 
-int search_replace_main_ver3(element_s *el)
+int search_replace_main_ver2(element_s *el)
 {
-	int i, c, fdw, offset, num_found = 0;
-	char *buf = NULL;
-	char *file;
-	char *base;
-	char *find;
-	char *replace = El_search_replace_replace(el);
-	char tmp_file[] = TMP_SEARCH_REPLACE_FILE;
-	FILE *fp, *fpw;
-	struct stat file_stat;
+	int i;
+	char *base = alloc_base_dir(el);
 
-
-	if ((find = El_search_replace_find(el)) == NULL) {
-		Nprint_h_err("No find string specified.");
-		return -1;
-	}
-
-	if ((file = El_search_replace_file(el)) == NULL) {
-		Nprint_h_err("No file specified.");
-		xfree(find);
-		return -1;
-	}
-
-	base = alloc_base_dir_new(el);
-
-	if (change_current_dir(base)) {
-		xfree(base);
-		xfree(find);
-		xfree(file);
-		return -1;
-	}
-
-	Nprint_h("Searching %s (%s)",  file, base);
-	Nprint_h("    for \"%s\"", find);
-	Nprint_h("    and replacing with \"%s\".",
-		replace ? replace : "");
+	i = search_replace_main(el, base);
 
 	xfree(base);
 
+	return i;
+}
 
-	if ((fp = fopen(file, "r")) == NULL) {
-		Nprint_h_err("Opening %s failed: %s",
-			file, strerror(errno));
-		xfree(find);
-		xfree(file);
-		return -1;
+int search_replace_main_ver3(element_s *el)
+{
+	int i;
+	char *base = alloc_base_dir_new(el);
 
-	}
+	i = search_replace_main(el, base);
 
-	if (fstat(fileno(fp), &file_stat)) {
-		Nprint_h_err("Error fstat()ing %s: %s",
-			file, strerror(errno));
-		xfree(find);
-		xfree(file);
-		return -1;
-	}
+	xfree(base);
 
-	if ((fdw = mkstemp(tmp_file)) == -1) {
-		Nprint_h_err("Error opening temporary file: %s",
-			strerror(errno));
-		xfree(find);
-		xfree(file);
-		return -1;
-	}
-	
-
-	if ((fpw = fdopen(fdw, "w")) == NULL) {
-		Nprint_h_err("Opening %s failed: %s",
-			tmp_file, strerror(errno));
-		xfree(find);
-		xfree(file);
-		return -1;
-	}
-
-	offset = (int)strlen(find) - 1;
-
-	buf = xmalloc(strlen(find) + 1);
-
-	for (i = 0; i < (int)(strlen(find)) + 1; i++) {
-		buf[i] = '\0';
-	}
-
-	while ((c = getc(fp)) != EOF) {
-		/* Shift to left, adding c at the end. */
-		i = offset;
-		while ((buf[i+1])) {
-			buf[i] = buf[i+1];
-			i++;
-		}
-		buf[i] = c;
-
-		if (strcmp(buf + offset, find) == 0) {
-			num_found++;
-			fseek(fpw, (long)-strlen(find) + 1, SEEK_CUR);
-
-			if (replace) {
-				fprintf(fpw, "%s", replace);
-			}
-
-		} else {
-			putc(c, fpw);
-		}
-
-		if (offset > 0)
-			offset--;
-	}
-
-	xfree(buf);
-
-	fclose(fpw);
-	fclose(fp);
-
-	Nprint_h("Made %d change%s.",
-		num_found, num_found != 1 ? "s" : ""); /* :-) */
-
-	if (execute_command("mv -f %s %s", tmp_file, file)) {
-		Nprint_h_err("System command for moving %s to %s failed.",
-			tmp_file, file);
-		xfree(find);
-		xfree(file);
-		return -1;
-	}
-
-	/* Changing the file mode. */
-	if (chmod(file, file_stat.st_mode)) {
-		Nprint_h_err("chmod(%s) failed: %s",
-			file, strerror(errno));
-		xfree(find);
-		xfree(file);
-		return -1;
-	}
-
-	xfree(find);
-	xfree(file);
-
-	return 0;
+	return i;
 }
 
 
