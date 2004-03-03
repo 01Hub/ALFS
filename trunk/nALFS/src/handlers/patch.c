77e276de058def7bc1bad441c77d8a070e3639a8
kpfleming <kpfleming@7d5e3409-5ede-0310-83dd-dbcb3a32f907>
2004-03-03 02:12:30 +0000

    convert to new parsing system

--- trunk/nALFS/src/handlers/patch.c
+++ trunk/nALFS/src/handlers/patch.c
@@ -43,43 +43,178 @@
 #include "backend.h"
 
 
+enum {
+	PATCH_BASE,
+	PATCH_PREFIX,
+	PATCH_PARAM,
+	PATCH_REFERENCE,
+	PATCH_FILE,
+	PATCH_MODE,
+	PATCH_PATH_STRIP,
+};
+
+struct patch_data {
+	char *base;
+	char *prefix;
+	char *param;
+	int param_seen;
+	int reference_count;
+	char **references;
+	char *file;
+	const element_s *digest;
+	int forward_mode;
+	int path_strip;
+};
+
+static int patch_setup(element_s * const element)
+{
+	struct patch_data *data;
+
+	if ((data = xmalloc(sizeof(struct patch_data))) == NULL)
+		return -1;
+
+	data->file = NULL;
+	data->param = xstrdup(" ");
+	data->param_seen = 0;
+	data->prefix = xstrdup("");
+	data->reference_count = 0;
+	data->references = NULL;
+	data->digest = NULL;
+	data->base = NULL;
+	data->path_strip = 1;
+	data->forward_mode = 1;
+	element->handler_data = data;
+
+	return 0;
+}
+
+static void patch_free(const element_s * const element)
+{
+	struct patch_data *data = (struct patch_data *) element->handler_data;
+	int i;
+
+	xfree(data->base);
+	xfree(data->file);
+	xfree(data->prefix);
+	xfree(data->param);
+	if (data->reference_count > 0) {
+		for (i = 0; i < data->reference_count; i++)
+			xfree(data->references[i]);
+		xfree(data->references);
+	}
+	xfree(data);
+}
+
+static int patch_attribute(const element_s * const element,
+			   const struct handler_attribute * const attr,
+			   const char * const value)
+{
+	struct patch_data *data = (struct patch_data *) element->handler_data;
+
+	switch (attr->private) {
+	case PATCH_BASE:
+		if (data->base) {
+			Nprint_err("<%s>: cannot specify \"base\" more than once.", element->handler->name);
+			return 1;
+		}
+		data->base = xstrdup(value);
+		return 0;
+	case PATCH_MODE:
+		if (!strcmp(value, "forward")) {
+			data->forward_mode = 1;
+			return 0;
+		} else if (!strcmp(value, "reverse")) {
+			data->forward_mode = 0;
+			return 0;
+		} else {
+			Nprint_err("<%s>: \"mode\" must be \"forward\" or \"reverse\".", element->handler->name);
+			return 1;
+		}
+	case PATCH_PATH_STRIP:
+		data->path_strip = atoi(value);
+		return 0;
+	default:
+		return 1;
+	}
+}
+
+static int patch_parameter(const element_s * const element,
+			   const struct handler_parameter * const param,
+			   const char * const value)
+{
+	struct patch_data *data = (struct patch_data *) element->handler_data;
+
+	switch (param->private) {
+	case PATCH_BASE:
+		if (data->base) {
+			Nprint_err("<%s>: cannot specify \"base\" more than once.", element->handler->name);
+			return 1;
+		}
+		data->base = xstrdup(value);
+		return 0;
+	case PATCH_PREFIX:
+		append_str(&data->prefix, value);
+		append_str(&data->prefix, " ");
+		return 0;
+	case PATCH_PARAM:
+		append_str(&data->param, value);
+		append_str(&data->param, " ");
+		data->param_seen = 1;
+		return 0;
+	case PATCH_FILE:
+		if (data->file) {
+			Nprint_err("<%s>: cannot specify <file> more than once.", element->handler->name);
+			return 1;
+		}
+		data->file = xstrdup(value);
+		return 0;
+	case PATCH_REFERENCE:
+		data->reference_count++;
+		if ((data->references = xrealloc(data->references,
+						 sizeof(data->references[0]) * (data->reference_count))) == NULL) {
+			Nprint_err("xrealloc() failed: %s", strerror(errno));
+			return -1;
+		}
+		data->references[(data->reference_count - 1)] = xstrdup(value);
+		return 0;
+	default:
+		return 1;
+	}
+}
+
+static int patch_valid_data(const element_s * const element)
+{
+	struct patch_data *data = (struct patch_data *) element->handler_data;
+
+	if (!data->param_seen) {
+		Nprint_err("<%s>: at least one <param> must be specified.", element->handler->name);
+		return 0;
+	}
+
+	return 1;
+}
+
 #if HANDLER_SYNTAX_2_0
 
 static const struct handler_parameter patch_parameters_v2[] = {
-	{ .name = "base" },
-	{ .name = "param" },
+	{ .name = "base", .private = PATCH_BASE },
+	{ .name = "param", .private = PATCH_PARAM },
 	{ .name = NULL }
 };
 
-static int patch_main_ver2(const element_s * const el)
+static int patch_main_ver2(const element_s * const element)
 {
+	struct patch_data *data = (struct patch_data *) element->handler_data;
 	int status;
-	char *base;
-	char *parameters = NULL;
 
-
-	if (append_param_elements(&parameters, el) == NULL) {
-		Nprint_h_err("No patch parameters specified.");
+	if (change_to_base_dir(element, data->base, 1))
 		return -1;
-	}
-
-	base = alloc_base_dir(el);
-
-	if (change_current_dir(base)) {
-		xfree(base);
-		xfree(parameters);
-		return -1;
-	}
 	
-	Nprint_h("Patching in %s", base);
-	Nprint_h("    patch %s", parameters);
+	Nprint_h("Patching:");
+	Nprint_h("    patch%s", data->param);
 
-	if ((status = execute_command(el, "patch %s", parameters))) {
+	if ((status = execute_command(element, "patch%s", data->param)))
 		Nprint_h_err("Patching failed.");
-	}
-	
-	xfree(base);
-	xfree(parameters);
 
 	return status;
 }
@@ -90,188 +225,156 @@ static int patch_main_ver2(const element_s * const el)
 #if HANDLER_SYNTAX_3_0 || HANDLER_SYNTAX_3_1
 
 static const struct handler_parameter patch_parameters_v3[] = {
-	{ .name = "prefix" },
-	{ .name = "param" },
+	{ .name = "prefix", .private = PATCH_PREFIX },
+	{ .name = "param", .private = PATCH_PARAM },
 	{ .name = NULL }
 };
 
 static const struct handler_attribute patch_attributes_v3[] = {
-	{ .name = "base" },
+	{ .name = "base", .private = PATCH_BASE },
 	{ .name = NULL }
 };
 
-static int patch_main_ver3(const element_s * const el)
+static int patch_main_ver3(const element_s * const element)
 {
+	struct patch_data *data = (struct patch_data *) element->handler_data;
 	int status;
-	char *parameters = NULL;
-	char *command;
+	char *command = NULL;
 
-
-	if (change_to_base_dir(el, attr_value("base", el), 1))
+	if (change_to_base_dir(element, data->base, 1))
 		return -1;
-
-	if (append_param_elements(&parameters, el) == NULL) {
-		Nprint_h_err("No patch parameters specified.");
-		return -1;
-	}
+	
+	append_str(&command, data->prefix);
+	append_str(&command, "patch");
+	append_str(&command, data->param);
 
 	Nprint_h("Patching");
-	command = xstrdup("");
-
-	append_prefix_elements(&command, el);
-
-	/* the trailing space on the string below is important, since
-	   append_param_elements will not have put a space at the
-	   beginning of the parameters string (since it was NULL to
-	   begin with)
-	*/
-
-	append_str(&command, "patch ");
-
-	append_str(&command, parameters);
-
 	Nprint_h("    %s", command);
 
-	if ((status = execute_command(el, command))) {
+	if ((status = execute_command(element, command)))
 		Nprint_h_err("Patching failed.");
-	}
 	
 	xfree(command);
-	xfree(parameters);
 
 	return status;
 }
 
 #endif /* HANDLER_SYNTAX_3_0 || HANDLER_SYNTAX_3_1 */
 
-
 #if HANDLER_SYNTAX_3_2
 
 static const struct handler_parameter patch_parameters_v3_2[] = {
-	{ .name = "prefix" },
-	{ .name = "param" },
-	{ .name = "digest" },
-	{ .name = "reference" },
-	{ .name = "file" },
+	{ .name = "prefix", .private = PATCH_PREFIX },
+	{ .name = "param", .private = PATCH_PARAM },
+	{ .name = "reference", .private = PATCH_REFERENCE },
+	{ .name = "file", .private = PATCH_FILE },
 	{ .name = NULL }
 };
 
 static const struct handler_attribute patch_attributes_v3_2[] = {
-	{ .name = "base" },
-	{ .name = "mode" },
-	{ .name = "path_strip" },
+	{ .name = "base", .private = PATCH_BASE },
+	{ .name = "mode", .private = PATCH_MODE },
+	{ .name = "path_strip", .private = PATCH_PATH_STRIP },
 	{ .name = NULL }
 };
 
-static int patch_main_ver3_2(const element_s * const el)
+static int patch_valid_data_v3_2(const element_s * const element)
 {
+	struct patch_data *data = (struct patch_data *) element->handler_data;
+
+	if (!data->file) {
+		Nprint_err("<%s>: <file> must be specified.", element->handler->name);
+		return 0;
+	}
+
+	return 1;
+}
+
+static int patch_valid_child_v3_2(const element_s * const element,
+				  const element_s * const child)
+{
+	struct patch_data *data = (struct patch_data *) element->handler_data;
+
+	if (child->handler->type & HTYPE_DIGEST) {
+		if (data->digest) {
+			Nprint_err("<%s>: only one <digest> allowed.", element->handler->name);
+			return 0;
+		}
+
+		data->digest = child;
+		return 1;
+	}
+
+	return 0;
+}
+
+static int patch_main_ver3_2(const element_s * const element)
+{
+	struct patch_data *data = (struct patch_data *) element->handler_data;
         int status = -1;
-	char *file = NULL;
-	char *command = NULL;
 	char *digest = NULL;
 	char *digest_type = NULL;
 	char *decompressor = NULL;
-	char *mode;
-	char *path_strip;
 	struct stat file_stat;
 
-
-	if (change_to_base_dir(el, attr_value("base", el), 1))
+	if (change_to_base_dir(element, data->base, 1))
 		return -1;
 
-	if ((file = alloc_trimmed_param_value("file", el)) == NULL) {
-		Nprint_h_err("File name is missing.");
-		goto free_all_and_return;
+	if (data->digest) {
+		digest = data->digest->handler->alloc_data(data->digest, HDATA_COMMAND);
+		digest_type = data->digest->handler->alloc_data(data->digest, HDATA_VERSION);
 	}
 
-	alloc_element_digest(el, &digest, &digest_type);
+	/* Check if file exists. */
+	if ((stat(data->file, &file_stat))) {
+	        if ((errno == ENOENT) && (data->reference_count > 0)) {
+			int i;
 
-	/* Check if file exists and if not attempt to download it. */
-	if ((stat(file, &file_stat))) {
-		if (errno == ENOENT && first_param("reference", el) != NULL) {
-			int found = 0;
-			element_s *p;
-
-			Nprint_h_warn("File %s not found.", file);
+			Nprint_h_warn("File %s not found.", data->file);
 			Nprint_h("Trying to fetch it from <reference>...");
 
-			for (p = first_param("reference", el); p; p = next_param(p)) {
-				char *s;
-
-				if ((s = alloc_trimmed_str(p->content)) == NULL) {
-					Nprint_h_warn("Source empty.");
-					continue;
-				}
-
-				if (! get_url(s, file, digest, digest_type)) {
-					found = 1;
-				}
-				xfree(s);
-				if (found)
+			for (i = 0; i < data->reference_count; i++) {
+				if (!get_url(data->references[i],
+					     data->file,
+					     digest,
+					     digest_type))
 					break;
 			}
 
-			if (!found) {
-				Nprint_h_err("Unable to download file %s.",
-					     file);
-				goto free_all_and_return;
+			if (i < data->reference_count) {
+				status = 0;
+			} else {
+				Nprint_h_err("Unable to download file %s.", data->file);
 			}
 		} else {
-			Nprint_h_err("Unable to download file %s.", file);
-			goto free_all_and_return;
+			Nprint_h_err("Checking for %s failed:", data->file);
+			Nprint_h_err("    %s", strerror(errno));
 		}
-	} else if (digest && verify_digest(digest_type, digest, file)) {
-		Nprint_h_err("Wrong %s digest of file: %s", digest_type,
-			     file);
-		goto free_all_and_return;
+	} else if (digest && verify_digest(digest_type, digest, data->file)) {
+		Nprint_h_err("Wrong %s digest of file: %s", digest_type, data->file);
+	} else {
+		status = 0;
 	}
 
-	decompressor = alloc_decompress_command(get_compression_type(file));
+	if (status == 0) {
+		decompressor = alloc_decompress_command(get_compression_type(data->file));
+		if (decompressor == NULL)
+			decompressor = xstrdup("cat %s");
+		append_str(&decompressor, " | %spatch -p%d %s%s");
 
-	if (decompressor == NULL)
-		decompressor = xstrdup("cat %s");
-	append_str(&decompressor, " | %s");
+		Nprint_h("    %spatch -p%d %s%s", data->prefix, data->path_strip,
+			 data->forward_mode ? "-N" : "-R", data->param);
 
-	command = xstrdup("");
+		if ((status = execute_command(element, decompressor, data->file,
+					      data->prefix, data->path_strip,
+					      data->forward_mode ? "-N" : "-R", data->param)))
+			Nprint_h_err("Patching failed.");
 
-	append_prefix_elements(&command, el);
-
-	append_str(&command, "patch -p");
-
-	path_strip = attr_value("path_strip", el);
-	if (path_strip)
-		append_str(&command, path_strip);
-	else
-		append_str(&command, "1");
-	
-	mode = attr_value("mode", el);
-	if (mode) {
-		if (strcmp(mode, "forward") == 0)
-			append_str(&command, " -N");
-		else if (strcmp(mode, "reverse") == 0)
-			append_str(&command, " -R");
-		else {
-			Nprint_h_err("Unknown mode specified: %s", mode);
-			goto free_all_and_return;
-		}
-	} else
-		append_str(&command, " -N");
-
-	append_param_elements(&command, el);
-
-	Nprint_h("    %s", command);
-
-	if ((status = execute_command(el, decompressor, file,
-				      command))) {
-		Nprint_h_err("Patching failed.");
+		xfree(decompressor);
 	}
 	
-free_all_and_return:
-	xfree(decompressor);
 	xfree(digest_type);
 	xfree(digest);
-	xfree(file);
-	xfree(command);
 
 	return status;
 }
@@ -292,6 +395,10 @@ handler_info_s HANDLER_SYMBOL(info)[] = {
 		.main = patch_main_ver2,
 		.type = HTYPE_NORMAL,
 		.is_action = 1,
+		.setup = patch_setup,
+		.free = patch_free,
+		.valid_data = patch_valid_data,
+		.parameter = patch_parameter,
 	},
 #endif
 #if HANDLER_SYNTAX_3_0
@@ -304,6 +411,11 @@ handler_info_s HANDLER_SYMBOL(info)[] = {
 		.main = patch_main_ver3,
 		.type = HTYPE_NORMAL,
 		.is_action = 1,
+		.setup = patch_setup,
+		.free = patch_free,
+		.parameter = patch_parameter,
+		.valid_data = patch_valid_data,
+		.attribute = patch_attribute,
 	},
 #endif
 #if HANDLER_SYNTAX_3_1
@@ -317,6 +429,11 @@ handler_info_s HANDLER_SYMBOL(info)[] = {
 		.type = HTYPE_NORMAL,
 		.alloc_data = NULL,
 		.is_action = 1,
+		.setup = patch_setup,
+		.free = patch_free,
+		.valid_data = patch_valid_data,
+		.parameter = patch_parameter,
+		.attribute = patch_attribute,
 	},
 #endif
 #if HANDLER_SYNTAX_3_2
@@ -330,6 +447,12 @@ handler_info_s HANDLER_SYMBOL(info)[] = {
 		.type = HTYPE_NORMAL,
 		.is_action = 1,
 		.alternate_shell = 1,
+		.setup = patch_setup,
+		.free = patch_free,
+		.valid_data = patch_valid_data_v3_2,
+		.parameter = patch_parameter,
+		.attribute = patch_attribute,
+		.valid_child = patch_valid_child_v3_2,
 	},
 #endif
 	{
