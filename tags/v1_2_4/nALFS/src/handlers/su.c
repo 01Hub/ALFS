e801155343308a3b3a896123eaa72b64858b8677
unknown <unknown@7d5e3409-5ede-0310-83dd-dbcb3a32f907>
2004-07-04 05:51:11 +0000

    This commit was manufactured by cvs2svn to create tag 'v1_2_4'.

---
+++ tags/v1_2_4/nALFS/src/handlers/su.c
@@ -0,0 +1,227 @@
+/*
+ *  su.c - Handler.
+ * 
+ *  Copyright (C) 2002
+ *  
+ *  Neven Has <haski@sezampro.yu>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <pwd.h>
+#include <grp.h>
+#include <dlfcn.h>
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#define MODULE_NAME su
+#include <nALFS.h>
+
+#include "handlers.h"
+#include "parser.h"
+#include "nprint.h"
+#include "nalfs-core.h"
+#include "backend.h"
+#include "utility.h"
+
+
+#if HANDLER_SYNTAX_2_0
+
+static INLINE int set_supplementary_groups(const char *user)
+{
+	int i;
+	size_t size = 0;
+	gid_t *list = NULL;
+	struct group *gr;
+	FILE *fp;
+
+
+	if ((fp = fopen("/etc/group", "r")) == NULL) {
+		Nprint_h_err("Unable to open /etc/group: %s",
+			strerror(errno));
+		return -1;
+	}
+
+	/* getgrnam() is failing in chroot() */
+	while ((gr = fgetgrent(fp))) {
+		for (i = 0; gr->gr_mem[i]; i++) {
+			if (strcmp(gr->gr_mem[i], user) == 0) {
+				size++;
+				list = xrealloc(list, size);
+				list[size-1] = gr->gr_gid;
+
+				break;
+			}
+		}
+	}
+
+	fclose(fp);
+
+	if (size == 0) {
+		return 0;
+	}
+
+	if (setgroups(size, list)) {
+		Nprint_h_err("Unable to set supplementary group IDs: %s",
+			strerror(errno));
+		return -1;
+	}
+
+	xfree(list);
+
+	return 0;
+}
+
+static INLINE int change_to_user(const char *user)
+{
+	struct passwd *pw;
+
+
+	setpwent();
+
+	/* getpwnam() is failing in chroot() */
+	while ((pw = getpwent())) {
+		if (strcmp(pw->pw_name, user) == 0) {
+			break;
+		}
+	}
+
+	endpwent();
+
+	if (pw == NULL) {
+		Nprint_h_err("User %s doesn't exist.", user);
+		return -1;
+	}
+
+	Nprint_h("Changing to user \"%s\".", pw->pw_name);
+
+	if (set_supplementary_groups(user)) {
+		return -1;
+	}
+
+	if (setgid(pw->pw_gid)) {
+		Nprint_h_err("Unable to set group ID: %s",
+			strerror(errno));
+		return -1;
+	}
+
+	if (setuid(pw->pw_uid)) {
+		Nprint_h_err("Unable to set user ID: %s",
+			strerror(errno));
+		return -1;
+	}
+
+	return 0;
+}
+
+
+static const char *su_parameters[] = { NULL };
+// char *HANDLER_SYMBOL(attributes)[] = { "user", NULL };
+
+static int su_main(element_s *el)
+{
+	int status;
+	pid_t su_pid, got_pid;
+        char *user = attr_value("user", el);
+
+	
+        if (user == NULL) {
+                Nprint_h_err("User not specified for su.");
+                return -1;
+	}
+
+	su_pid = fork();
+
+	if (su_pid == 0) { /* Child. */
+		Start_receiving_sigio();
+
+		if (change_to_user(user) == -1) {
+			exit(EXIT_FAILURE);
+		}
+
+		status = execute_children(el);
+
+		/* Nprint_h("Exited from su (%s).", pw->pw_name); */
+		
+		exit(status);
+
+	} else if (su_pid == -1) { /* Error. */
+		fatal_backend_error("fork() in su element failed.");
+	}
+
+	if ((got_pid = waitpid(su_pid, &status, WUNTRACED)) == -1) {
+		fatal_backend_error("waitpid() for %ld in su failed.",
+			(long)su_pid);
+	}
+
+	if (WIFEXITED(status)) { /* Child exited normally. */
+		status = WEXITSTATUS(status);
+
+	} else if (WIFSIGNALED(status)) {
+		Nprint_h_err("Su child (%ld) killed by signal %d%s.",
+			(long)got_pid, WTERMSIG(status),
+			WCOREDUMP(status) ? " (core dumped)" : "");
+		status = -1;
+
+	} else if (WIFSTOPPED(status)) {
+		Nprint_h_err("Su child (%ld) stopped by signal %d.",
+			(long)got_pid, WSTOPSIG(status));
+		status = -1;
+
+	} else {
+		Nprint_h_err("Su child (%ld) exited abnormaly.",
+			(long)got_pid);
+		status = -1;
+	}
+	
+	return status;
+}
+
+#endif /* HANDLER_SYNTAX_2_0 */
+
+
+/*
+ * Handlers' information.
+ */
+
+handler_info_s HANDLER_SYMBOL(info)[] = {
+#if HANDLER_SYNTAX_2_0
+	{
+		.name = "su",
+		.description = "Change user ID",
+		.syntax_version = "2.0",
+		.parameters = su_parameters,
+		.main = su_main,
+		.type = 0,
+		.alloc_data = NULL,
+		.is_action = 0,
+		.priority = 0
+	},
+#endif
+	{
+		NULL, NULL, NULL, NULL, NULL, 0, NULL, 0, 0
+	}
+};
---
+++ tags/v1_2_4/nALFS/src/win.c
@@ -0,0 +1,418 @@
+/*
+ *  win.c - Windows handling functions.
+ *
+ *  Copyright (C) 2001-2003
+ *
+ *  Neven Has <haski@sezampro.yu>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+#include <errno.h>
+#include <curses.h>
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include "utility.h"
+#include "nalfs-core.h"
+#include "options.h"
+
+#include "win.h"
+
+
+windows_s windows = {
+	.main 			= NULL,
+	.status 		= NULL,
+	.active 		= MAIN_WINDOW,
+	.max_lines 		= 0,
+	.max_cols 		= 0,
+	.middle_line_offset 	= 0
+};
+
+
+typedef struct msg_type_t {
+	msg_id_e id;		// Unique ID of the message type.
+	char character;		// Character to print before the message.
+	unsigned long attrs;	// Attributes for printing the above character.
+} msg_type_t;
+
+static const msg_type_t message_types[] = {
+	{ T_RAW, ' ', COL_WHITE },
+	{ T_INF, 'I', COL_CYAN },
+	{ T_HLP, 'H', COL_CYAN },
+	{ T_SYS, '-', COL_CYAN },
+	{ T_WAR, 'W', COL_CYAN_BOLD },
+	{ T_ERR, 'E', COL_RED_BOLD }
+};
+
+
+char msg_character(msg_id_e mid)
+{
+	ASSERT(mid <= T_ERR);
+
+	if (mid <= T_ERR) {
+		return message_types[mid].character;
+	}
+
+	return 'x';
+}
+
+unsigned long msg_attrs(msg_id_e mid)
+{
+	ASSERT(mid <= T_ERR);
+
+	if (mid <= T_ERR) {
+		return message_types[mid].attrs;
+	}
+
+	return COL_WHITE;
+}
+
+static INLINE void init_colors(void)
+{
+	if (has_colors()) {
+		start_color();
+
+		init_pair(COLP_CYAN, COLOR_CYAN, COLOR_BLACK);
+		init_pair(COLP_RED, COLOR_RED, COLOR_BLACK);
+		init_pair(COLP_GREEN, COLOR_GREEN, COLOR_BLACK);
+		init_pair(COLP_WHITE, COLOR_WHITE, COLOR_BLACK);
+	}
+}
+
+static void set_default_window_options(window_s *w)
+{
+	leaveok(w->name, TRUE);
+	scrollok(w->name, TRUE);
+	idlok(w->name, TRUE);
+	keypad(w->name, TRUE);
+	nodelay(w->name, TRUE);
+}
+
+
+
+static int main_window_lines(void)
+{
+	int lines;
+	int lines_with_offset;
+
+
+	lines = (*opt_windows_relation * (windows.max_lines - 3)) / 100;
+
+	lines_with_offset = lines + windows.middle_line_offset;
+
+	if (lines_with_offset < 2) {
+		lines_with_offset = 2;
+
+	} else if (lines_with_offset > windows.max_lines - 3 - 2) {
+		lines_with_offset = windows.max_lines - 3 - 2;
+	}
+
+	windows.middle_line_offset = lines_with_offset - lines;
+
+	return lines_with_offset;
+}
+
+
+
+static int refresh_main(int top_line)
+{
+	Xprefresh(windows.main->name, top_line, 0,
+		1, 1, windows.main->lines, windows.max_cols - 2);
+
+	return 0;
+}
+
+static window_s *create_main_window(int num_of_lines)
+{
+	window_s *w = xmalloc(sizeof *w);
+
+
+	w->ref = refresh_main;
+	w->lines = main_window_lines();
+	/* We're adding 1 just in case. */
+	w->name = newpad(num_of_lines + 1, windows.max_cols - 2);
+
+	if (w->name == NULL) {
+		Fatal_error("newpad() failed");
+	}
+
+	set_default_window_options(w);
+
+	return w;
+}
+
+
+
+static int refresh_status(int offset)
+{
+	int x, starting_top;
+	int top;
+
+
+	/* Get starting top line number. */
+	getmaxyx(windows.status->name, starting_top, x);
+	starting_top -= windows.status->lines;
+
+	top = starting_top - offset;
+
+	if (top < 0) {
+		top = 0;
+
+	} else if (top > starting_top) {
+		top = starting_top;
+	}
+
+	Xprefresh(windows.status->name, top, 0,
+		windows.max_lines - 1 - windows.status->lines, 1,
+		windows.max_lines - 2, windows.max_cols - 2);
+
+	return (starting_top - top); /* offset */
+}
+
+static INLINE window_s *create_status_window(void)
+{
+	int main_lines = main_window_lines();
+	window_s *w = xmalloc(sizeof *w);
+
+
+	w->ref = refresh_status;
+	w->lines = windows.max_lines - 3 - main_lines;
+	w->name = newpad(*opt_status_history + w->lines, windows.max_cols - 2);
+
+	if (w->name == NULL) {
+		Fatal_error("newpad() failed");
+	}
+
+	set_default_window_options(w);
+
+	Xwmove(w->name, *opt_status_history - 1, 0);
+
+	return w;
+}
+
+
+
+static INLINE void create_windows(int num_of_lines)
+{
+	getmaxyx(stdscr, windows.max_lines, windows.max_cols);
+
+	/* windows.active is defined in browse() */
+
+	windows.main = create_main_window(num_of_lines);
+	windows.status = create_status_window();
+}
+
+void start_display(void)
+{
+	initscr();
+	cbreak();
+	noecho();
+
+	scrollok(stdscr, FALSE);
+
+	init_colors();
+
+	create_windows(0);
+}
+
+static INLINE void delete_all_windows(void)
+{
+	delwin(windows.main->name);
+	xfree(windows.main);
+
+	delwin(windows.status->name);
+	xfree(windows.status);
+
+	clear();
+	refresh();
+}
+
+void end_display(void)
+{
+	delete_all_windows();
+
+	endwin();
+}
+
+void recreate_main_window(int num_of_lines)
+{
+	/* Clear the window in case the new one is smaller. */
+	werase(windows.main->name);
+	refresh_main(0);
+
+	delwin(windows.main->name);
+	xfree(windows.main);
+
+	windows.main = create_main_window(num_of_lines);
+}
+
+void resize_all_windows(void)
+{
+	int main_lines = main_window_lines();
+	int status_lines = windows.max_lines - 3 - main_lines;
+
+
+	windows.main->lines = main_lines;
+	windows.status->lines = status_lines;
+}
+
+static void fix_cursor_and_top(int *cursor, int *top, int lines)
+{
+	int window_lines = windows.main->lines;
+
+	if (cursor) {
+		if (*cursor < 0) {
+			*cursor = 0;
+		}
+		if (*cursor > lines - 1) {
+			*cursor = lines - 1;
+		}
+
+		while (*cursor > (*top + (window_lines - 1))) {
+			(*top)++;
+		}
+		while (*cursor < *top) {
+			(*top)--;
+		}
+	}
+
+	if (*top < 0) {
+		*top = 0;
+	}
+	if (*top > lines - window_lines) {
+		*top = lines - window_lines + 1;
+	}
+}
+
+static void change_value(int *i, int lines, int input)
+{
+	switch (input) {
+		case KEY_UP:
+			--(*i);
+			break;
+		case KEY_DOWN:
+			++(*i);
+			break;
+		case KEY_PPAGE:
+			*i -= windows.main->lines - 1;
+			break;
+		case KEY_NPAGE:
+			*i += windows.main->lines - 1;
+			break;
+		case KEY_HOME:
+			*i = 0;
+			break;
+		case KEY_END:
+			*i = lines - 1;
+			break;
+		case ' ':
+			break;
+	}
+}
+
+static void move_cursor(int *top, int *cursor, int lines, int input)
+{
+	if (cursor == NULL) {
+		change_value(top, lines, input);
+	} else {
+		change_value(cursor, lines, input);
+	}
+
+	fix_cursor_and_top(cursor, top, lines);
+}
+
+int tmp_window_driver(int lines, int *top, int *cursor)
+{
+	int input;
+	int old_cursor = 0;
+
+
+	if (cursor) {
+		move_cursor(top, cursor, lines, ' ');
+		Xmvwaddstr(windows.main->name, *cursor, 0, *opt_cursor);
+	}
+
+	windows.main->ref(*top);
+
+	while (1) {
+		if (cursor) {
+			old_cursor = *cursor;
+		}
+
+		input = get_key(windows.main->name);
+
+		switch (input) {
+			case KEY_UP:
+				move_cursor(top, cursor, lines, KEY_UP);
+				break;
+
+			case '\n':
+			case KEY_DOWN:
+				move_cursor(top, cursor, lines, KEY_DOWN);
+				break;
+
+			case MOD_CTRL('p'):
+			case KEY_PPAGE:
+				move_cursor(top, cursor, lines, KEY_PPAGE);
+				break;
+
+			case ' ':
+			case MOD_CTRL('n'):
+			case KEY_NPAGE:
+				move_cursor(top, cursor, lines, KEY_NPAGE);
+				break;
+
+			case KEY_HOME:
+				move_cursor(top, cursor, lines, KEY_HOME);
+				break;
+
+			case KEY_END:
+				move_cursor(top, cursor, lines, KEY_END);
+				break;
+
+			case 'q':
+			case KEY_LEFT:
+			case KEY_F(10):
+				return -1;
+		
+			default:
+				return input;
+		}
+
+		if (cursor) {
+			size_t i;
+			for (i = 0; i < strlen(*opt_cursor) + 1; ++i)
+				Xmvwaddch(
+				windows.main->name, old_cursor, i, ' ');
+
+			Xmvwaddstr(
+			windows.main->name, *cursor, 0, *opt_cursor);
+		}
+
+		windows.main->ref(*top);
+
+		refresh(); /* For moving the annoying '_' cursor. */
+	}
+
+	/* Never reached. */
+}
