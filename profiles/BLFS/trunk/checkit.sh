94977d19681292d98dad9f3ba8f507726458ca31
thomasp <thomasp@7d5e3409-5ede-0310-83dd-dbcb3a32f907>
2004-07-12 19:00:52 +0000

    fixed DTD validation error

--- profiles/BLFS/trunk/checkit.sh
+++ profiles/BLFS/trunk/checkit.sh
@@ -1,9 +1,1 @@
-
-xmllint -noent  -loaddtd -valid BLFS.xml 2>&1 | ${PAGER:-more}
-
-
-#export SP_CHARSET_FIXED=YES
-#export SP_ENCODING=XML
-
-#nsgmls -wxml -s  BLFS.xml 
-
+xmllint --noent --nonet --noout --xinclude --postvalid BLFS.xml
