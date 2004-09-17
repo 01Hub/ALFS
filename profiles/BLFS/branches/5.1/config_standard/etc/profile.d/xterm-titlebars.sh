99ea70af58db7784f49447aedf4b46c2317ddb45
kpfleming <kpfleming@7d5e3409-5ede-0310-83dd-dbcb3a32f907>
2004-09-17 03:28:35 +0000

    create branch for BLFS-5.1

---
+++ profiles/BLFS/branches/5.1/config_standard/etc/profile.d/xterm-titlebars.sh
@@ -0,0 +1,5 @@
+# The substring match ensures this will work for "xterm" and "xterm-xfree86".
+if [ "${TERM:0:5}" = "xterm" ]; then
+  PROMPT_COMMAND='echo -ne "\033]0;${USER}@${HOSTNAME} : ${PWD}\007"'
+  export PROMPT_COMMAND
+fi
