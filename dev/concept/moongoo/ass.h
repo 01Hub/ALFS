50b819013d0779efd71fe08d31fae867ec266e24
neocool <neocool@7d5e3409-5ede-0310-83dd-dbcb3a32f907>
2004-12-01 12:18:53 +0000

    * Support for two new syntaxes added: 	ASS - ALFS Simple Syntax 	nALFS legacy syntax

---
+++ dev/concept/moongoo/ass.h
@@ -0,0 +1,8 @@
+#ifndef __ASS_H__
+#define __ASS_H__
+
+#include <alfs.h>
+
+profile *ass_profile (xmlNodePtr node);
+
+#endif
---
+++ dev/concept/moongoo/nalfs.h
@@ -0,0 +1,8 @@
+#ifndef __NALFS_H__
+#define __NALFS_H__
+
+#include <alfs.h>
+
+profile *nalfs_profile (xmlNodePtr node);
+
+#endif
