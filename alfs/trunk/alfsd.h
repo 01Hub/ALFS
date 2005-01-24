c5f97b4b840dfbe2559b181a11b4b9642fd1cd6f
neocool <neocool@7d5e3409-5ede-0310-83dd-dbcb3a32f907>
2005-01-24 18:35:25 +0000

    Minor update.

--- alfs/trunk/alfsd.h
+++ alfs/trunk/alfsd.h
@@ -3,4 +3,9 @@
 
 #define PLUG_DIR	"./"
 
+role default_filter[4] = { NOEXECUTE, INTERACTIVE, TESTSUITE, 0 };
+char *paralell_filter[4] = { "configure-host", "clean", "mrproper", NULL };
+char *popt_pkg[2] = { "Glibc", NULL };
+char *popt_cmd[2] = { "PARALLELMFLAGS=-j", NULL };
+
 #endif
