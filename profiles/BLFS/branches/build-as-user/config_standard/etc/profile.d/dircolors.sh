b287691e40afddef441e88f7e2e884241a1413dc
thomasp <thomasp@7d5e3409-5ede-0310-83dd-dbcb3a32f907>
2005-05-23 18:41:20 +0000

    create branch to work on implemeting building as user, installing as root

---
+++ profiles/BLFS/branches/build-as-user/config_standard/etc/profile.d/dircolors.sh
@@ -0,0 +1,13 @@
+# Begin /etc/profile.d/dircolors.sh
+
+# Setup for /bin/ls to support color, the alias is in /etc/bashrc.
+if [ -f "/etc/dircolors" ] ; then
+        eval $(dircolors -b /etc/dircolors)
+ 
+        if [ -f "$HOME/.dircolors" ] ; then
+                eval $(dircolors -b $HOME/.dircolors)
+        fi
+fi
+alias ls='ls --color=auto'
+
+# End /etc/profile.d/dircolors.sh
