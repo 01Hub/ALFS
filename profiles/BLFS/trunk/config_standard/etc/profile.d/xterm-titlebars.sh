9e8a0a59a17faea512d3f73f0baee07de638deb2
thomasp <thomasp@7d5e3409-5ede-0310-83dd-dbcb3a32f907>
2005-06-29 22:21:53 +0000

    * Updates to Chapter 3 section files * ispell dictionaries are now installed to /usr/share/ispell * Package upgrades:  - autofs-4.1.4  - openssl-0.9.7g  - nano-1.2.5  - joe-3.3  - pcre-6.1  - libgsf-1.12.0  - freetype-2.1.10  - fontconfig-2.3.2  - hd2u-1.0.0  - gtk-doc-1.3  - intltool-0.33  - HTML Tidy-050531  - xscreensaver-4.21

--- profiles/BLFS/trunk/config_standard/etc/profile.d/xterm-titlebars.sh
+++
@@ -1,9 +0,0 @@
-# Begin /etc/profile.d/xterm-titlebars.sh
-
-# The substring match ensures this will work for "xterm" and "xterm-xfree86".
-if [ "${TERM:0:5}" = "xterm" ]; then
-  PROMPT_COMMAND='echo -ne "\033]0;${USER}@${HOSTNAME} : ${PWD}\007"'
-  export PROMPT_COMMAND
-fi
-
-# End /etc/profile.d/xterm-titlebars.sh
