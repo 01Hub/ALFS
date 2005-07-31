4091180ff2c57dfbb66a63f95dd189eba71a5e42
thomasp <thomasp@7d5e3409-5ede-0310-83dd-dbcb3a32f907>
2005-07-31 20:09:51 +0000

    * Package Upgrades:  - avifile-0.7.43  - MPlayer-1.0pre7  - cdrdao-1.2.0  - ghostscript-8.51  - docbook sgml-dtd-4.4  - docbook dsssl-stylesheets-1.79  - docbook xml-dtd-4.4  - docbook xsl-stylesheets-1.68.1

---
+++ profiles/BLFS/trunk/config_standard/scripts/old-dtd-compat.sh
@@ -0,0 +1,24 @@
+CURRENT_DTD_VERSION=4.4
+for DTDVERSION in 4.1.2 4.2 4.3
+do
+  xmlcatalog --noout --add "public" \
+    "-//OASIS//DTD DocBook XML V$DTDVERSION//EN" \
+    "http://www.oasis-open.org/docbook/xml/$DTDVERSION/docbookx.dtd" \
+    /etc/xml/docbook
+  xmlcatalog --noout --add "rewriteSystem" \
+    "http://www.oasis-open.org/docbook/xml/$DTDVERSION" \
+    "file:///usr/share/xml/docbook/xml-dtd-$CURRENT_DTD_VERSION" \
+    /etc/xml/docbook
+  xmlcatalog --noout --add "rewriteURI" \
+    "http://www.oasis-open.org/docbook/xml/$DTDVERSION" \
+    "file:///usr/share/xml/docbook/xml-dtd-$CURRENT_DTD_VERSION" \
+    /etc/xml/docbook
+  xmlcatalog --noout --add "delegateSystem" \
+    "http://www.oasis-open.org/docbook/xml/$DTDVERSION/" \
+    "file:///etc/xml/docbook" \
+    /etc/xml/catalog
+  xmlcatalog --noout --add "delegateURI" \
+    "http://www.oasis-open.org/docbook/xml/$DTDVERSION/" \
+    "file:///etc/xml/docbook" \
+    /etc/xml/catalog
+done
