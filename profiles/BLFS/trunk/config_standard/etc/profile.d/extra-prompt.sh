cacc5d82186e1b3ec8d406bf6c72dd95edcef1d0
jbeckers <jbeckers@7d5e3409-5ede-0310-83dd-dbcb3a32f907>
2005-09-17 08:13:11 +0000

    patches from torsten

--- profiles/BLFS/trunk/config_standard/etc/profile.d/extra-prompt.sh
+++ profiles/BLFS/trunk/config_standard/etc/profile.d/extra-prompt.sh
@@ -1,6 +1,6 @@
 # Begin /etc/profile.d/extra-prompt.sh
 
 PROMPT_COMMAND="echo -ne '\e[1m${USER}@${HOSTNAME} : ${PWD}\e[0m\a'"
-export PROMPT_COMMAND
+#export PROMPT_COMMAND
 
 # End /etc/profile.d/extra-prompt.sh
