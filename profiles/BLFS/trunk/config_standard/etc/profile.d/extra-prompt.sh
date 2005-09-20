3ceb320be3293312fee6f0205fe3a81035cf3b21
jbeckers <jbeckers@7d5e3409-5ede-0310-83dd-dbcb3a32f907>
2005-09-20 15:51:16 +0000

    extra-prompt.sh fix

--- profiles/BLFS/trunk/config_standard/etc/profile.d/extra-prompt.sh
+++ profiles/BLFS/trunk/config_standard/etc/profile.d/extra-prompt.sh
@@ -1,6 +1,10 @@
 # Begin /etc/profile.d/extra-prompt.sh
 
-PROMPT_COMMAND="echo -ne '\e[1m${USER}@${HOSTNAME} : ${PWD}\e[0m\a'"
+PROMPT_COMMAND='echo -ne "\e[1m${USER}@${HOSTNAME} : ${PWD}\e[0m\a"'
+
+# export is disabled as this is an example
+# if you want to use PROMPT_COMMAND remember to edit PS1, too 
+
 #export PROMPT_COMMAND
 
 # End /etc/profile.d/extra-prompt.sh
