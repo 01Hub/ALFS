278651c43fb099205499a2338d376267a0294350
gerard <gerard@7d5e3409-5ede-0310-83dd-dbcb3a32f907>
2005-11-25 21:41:25 +0000

    Added comments

--- alfs-POC/client-server/server.c
+++ alfs-POC/client-server/server.c
@@ -15,22 +15,40 @@ main ()
 	struct sockaddr_in servaddr;
 	char buff[MAXLINE];
 	
+	/* setup socket descriptor */
 	listenfd = socket(AF_INET, SOCK_STREAM, 0);
-	
+
+	/* set structure to zero */	
 	bzero(&servaddr, sizeof(servaddr));
+
+	/* set to IPv4 */
 	servaddr.sin_family = AF_INET;
+
+	/* listen on any IP address port 1234 */
 	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
 	servaddr.sin_port = htons(1234);
-	
+
+	/* bind to socket and listen for incoming requests */	
 	bind(listenfd, (SA *) &servaddr, sizeof(servaddr));
 	listen(listenfd, LISTENQ);
-	
+
+	/* endless loop. Service one client at a time, then go back
+	   to listening */	
 	for ( ; ; ) {
+		/* process goes to sleep after 'accept' until a client
+		   connection arrives and is accepted after TCP's
+		   three-way handshake*/
 		connfd = accept(listenfd, (SA *) NULL, NULL);
-		
+
+		/* fill buffer with data to send to client */		
 		snprintf(buff, sizeof(buff), "This is a test\r\n");
+
+		/* send buffer to client */
 		write(connfd, buff, strlen(buff));
-	
+
+		/* close connection, restart while loop, goes back to sleep
+		  after accept	*/
 		close(connfd);
 	}
 }
+
